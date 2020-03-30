0e331294d32a6159b4b367a13e74f30a83174de4
Shubham Chaudhary <shubham@chaudhary.xyz>
2020-03-29 21:32:36 -0400

    Add percentage to the numbers

--- lib/pages/home/streaks_widget.dart
+++ lib/pages/home/streaks_widget.dart
@@ -16,10 +16,10 @@ class StreaksWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Container(
-      padding: EdgeInsets.symmetric(vertical: TEXT_PADDING, horizontal: 2 * TEXT_PADDING),
+      padding: EdgeInsets.symmetric(vertical: 2 * TEXT_PADDING, horizontal: 2 * TEXT_PADDING),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: widgetsList(context),
       ),
     );
@@ -30,10 +30,10 @@ class StreaksWidget extends StatelessWidget {
     var updatedAtDateTimeString = DateFormat.yMMMMEEEEd().format(updatedAtDateTime.toLocal()) + " " + DateFormat.Hms().format(updatedAtDateTime.toLocal());
 //    updatedAtDateTimeString = DateFormat.E().format(updatedAtDateTime.toLocal());
     var output = <Widget>[
-        showStat("Cases", data.Cases, CupertinoColors.activeOrange),
-        showStat("Active", data.Active, CupertinoColors.systemYellow),
-        showStat("Deaths", data.Deaths, CupertinoColors.destructiveRed),
-        showStat("Recovered", data.Recovered, CupertinoColors.activeGreen),
+        showStat("Cases", data.Cases, null, CupertinoColors.activeOrange),
+        showStat("Active", data.Active, (100.0 * data.Active)/data.Cases, CupertinoColors.systemYellow),
+        showStat("Deaths", data.Deaths, (100.0 * data.Deaths)/data.Cases, CupertinoColors.destructiveRed),
+        showStat("Recovered", data.Recovered, (100.0 * data.Recovered)/data.Cases, CupertinoColors.activeGreen),
         //showStat("Updated At", updatedAtDateTimeString),
         Text(
           "Updated At: ${updatedAtDateTimeString}",
@@ -46,16 +46,18 @@ class StreaksWidget extends StatelessWidget {
     return output;
   }
 
-  Padding showStat(title, numbers, color) {
+  Widget showStat(title, numbers, percentage, color) {
     var normalColor = isDark ? CupertinoColors.white : CupertinoColors.black;
     var textColor = color == null ? normalColor : color;
+    var percentageText = percentage == null ? "" : "${percentage.toStringAsFixed(1)}%";
     return Padding(
-        padding: const EdgeInsets.only(bottom: TEXT_PADDING),
+        padding: const EdgeInsets.only(bottom: 4 * TEXT_PADDING),
         child: Column(
         children: <Widget>[
           Text(
             "${title.toUpperCase()}",
             style: TextStyle(
+              letterSpacing: 1.5,
               fontWeight: FontWeight.w500,
               fontSize: 20,
 //              color: isDark ? CupertinoColors.darkBackgroundGray : CupertinoColors.extraLightBackgroundGray,
@@ -63,13 +65,31 @@ class StreaksWidget extends StatelessWidget {
               color: normalColor,
             ),
           ),
-          Text(
-            "$numbers",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 60,
-              color: textColor,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "$numbers",
+                style: TextStyle(
+                  letterSpacing: 4.5,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 60,
+                  color: textColor,
+                ),
+                ),
+              Padding(
+                padding: const EdgeInsets.only(top: 1.5 * TEXT_PADDING),
+                child: Text(
+                  percentageText,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 10,
+                    color: normalColor,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
